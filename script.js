af10a57c7257f9167dca86c5fe5522d1e9db92a0
むいにゃん <hngrmi@gmail.com>
2019-10-29 02:23:30 +0900

    存在しない譜面のNO PLAYを表示しないように修正

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 (function () {
 
+    const INDEX_TITLE=1;
+    const INDEX_BEGINNER_CLEARTYPE=10;
+    const INDEX_NORMAL_CLEARTYPE=17;
+    const INDEX_HYPER_CLEARTYPE=24;
+    const INDEX_ANOTHER_CLEARTYPE=31;
+    const INDEX_LEGGENDARIA_CLEARTYPE=38;
+    const INDEX_BEGINNER_DIFFICULTY=5;
+    const INDEX_NORMAL_DIFFICULTY=12;
+    const INDEX_HYPER_DIFFICULTY=19;
+    const INDEX_ANOTHER_DIFFICULTY=26;
+    const INDEX_LEGGENDARIA_DIFFICULTY=33;
+
     let inputFile = document.getElementById('file');
     let reader = new FileReader();
 
@@ -12,6 +24,25 @@
         reader.readAsText(file);
     }
 
+    function cleansingClearType(row) {
+        if (row[INDEX_BEGINNER_DIFFICULTY] == 0) {
+            row[INDEX_BEGINNER_CLEARTYPE] = "";
+        }
+        if (row[INDEX_NORMAL_DIFFICULTY] == 0) {
+            row[INDEX_NORMAL_CLEARTYPE] = "";
+        }
+        if (row[INDEX_HYPER_DIFFICULTY] == 0) {
+            row[INDEX_HYPER_CLEARTYPE] = "";
+        }
+        if (row[INDEX_ANOTHER_DIFFICULTY] == 0) {
+            row[INDEX_ANOTHER_CLEARTYPE] = "";
+        }
+        if (row[INDEX_LEGGENDARIA_DIFFICULTY] == 0) {
+            row[INDEX_LEGGENDARIA_CLEARTYPE] = "";
+        }
+        return row;
+    }
+
     function fileLoad() {
         let text = reader.result;
         let table = text.split(/[\r\n]+/).map(s => s.split(","))
@@ -22,14 +53,15 @@
         console.log(rows);
 
         let rows_html = (
-            rows.filter(r => r[1] !== undefined)
+            rows.filter(r => r[INDEX_TITLE] !== undefined)
+            .map(cleansingClearType)
             .map(r => `<tr>
-                    <td>${r[1]}</td>
-                    <td data-cleartype="${r[10]}">${r[10]}</td>
-                    <td data-cleartype="${r[17]}">${r[17]}</td>
-                    <td data-cleartype="${r[24]}">${r[24]}</td>
-                    <td data-cleartype="${r[31]}">${r[31]}</td>
-                    <td data-cleartype="${r[38]}">${r[38]}</td>
+                    <td>${r[INDEX_TITLE]}</td>
+                    <td data-cleartype="${r[INDEX_BEGINNER_CLEARTYPE]}">${r[INDEX_BEGINNER_CLEARTYPE]}</td>
+                    <td data-cleartype="${r[INDEX_NORMAL_CLEARTYPE]}">${r[INDEX_NORMAL_CLEARTYPE]}</td>
+                    <td data-cleartype="${r[INDEX_HYPER_CLEARTYPE]}">${r[INDEX_HYPER_CLEARTYPE]}</td>
+                    <td data-cleartype="${r[INDEX_ANOTHER_CLEARTYPE]}">${r[INDEX_ANOTHER_CLEARTYPE]}</td>
+                    <td data-cleartype="${r[INDEX_LEGGENDARIA_CLEARTYPE]}">${r[INDEX_LEGGENDARIA_CLEARTYPE]}</td>
                 </tr>`)
             .join("\n")
         );
